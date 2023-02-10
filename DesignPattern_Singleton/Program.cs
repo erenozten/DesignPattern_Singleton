e819856428dc049d6d21df5270ba87a1fadb2724
Eren ÖZTEN <erenozten@gmail.com>
2023-02-10 20:49:34 +0300

    Init

---
+++ DesignPattern_Singleton/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace DesignPattern_Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // client
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            // s1 ve s2 objesi aynıdır:
+            if (s1 == s2)
+            {
+                Console.WriteLine("Objeler aynı");
+            }
+            else
+            {
+                Console.WriteLine("Objeler farklı");
+            }
+        }
+    }
+}
---
+++ DesignPattern_Singleton/Singleton.cs
@@ -0,0 +1,26 @@
+﻿
+namespace DesignPattern_Singleton
+{
+    public sealed class Singleton
+    {
+        public string Name { get; set; }
+
+        private Singleton() { }
+
+        private static Singleton _instance;
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+        
+        public void someBusinessLogic()
+        {
+            // business logic buraya
+        }
+    }
+}
